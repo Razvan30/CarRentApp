28a2633230c5c0b6cfb0e1e74c7487f88e5f95a4
Ivascu Razvan <razvan_ivascu@yahoo.com>
2022-02-27 20:32:17 +0200

    Add button with label

--- CarRentApp/CarRentApp/MainForm.Designer.cs
+++ CarRentApp/CarRentApp/MainForm.Designer.cs
@@ -29,12 +29,46 @@ namespace CarRentApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lCar = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lCar
+            // 
+            this.lCar.AutoSize = true;
+            this.lCar.Location = new System.Drawing.Point(89, 30);
+            this.lCar.Name = "lCar";
+            this.lCar.Size = new System.Drawing.Size(23, 13);
+            this.lCar.TabIndex = 0;
+            this.lCar.Text = "Car";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(243, 20);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lCar);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lCar;
+        private System.Windows.Forms.Button btnShow;
     }
 }
